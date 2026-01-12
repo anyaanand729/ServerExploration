e03c7a7415089b255ef78ad2757b27919627e3d9
anyaanand729 <126aanand@frhsd.com>
2026-01-12 11:35:44 -0500

    secondPush

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const[ ,setUsers] = useState<string>('');
+
+
+    //GET
+  useEffect(() => {
+      const API_URL = "https://localhost:3000/users"
+
+      fetch(API_URL)
+          .then((response) => {
+              if(!response.ok){
+                  throw new Error(`HTTPS error| status ${response.status}`)
+              }
+              return response.json()
+          })
+          .then((data) => {
+              setUsers(data);
+          })
+          .finally(() => {
+
+          });
+  }, [])
+
+    //POST
+    useEffect(() => {
+        const API_URL = "https://localhost:3000/users"
+
+        fetch(API_URL, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+
+        })
+    })
+
+    //PUT
+
+    //DELETE
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
